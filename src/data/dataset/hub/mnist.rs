fda4bfac96c7c35516d834a5350df13f68708820
Ferran Sanchez Llado <ferransll@gmail.com>
2023-07-29 12:59:51 +0200

    Finished MNIST Dataset
    - Implemented Dataset and IterableDataset trait for MNIST.
    - Added tests for all steps in the creation of the MNIST dataset.
    - Implemented a basic framework to avoid downloading the dataset N times and allowing parallel testing.
    - Fixed small bug in check exists.

--- src/data/dataset/hub/mnist.rs
+++ src/data/dataset/hub/mnist.rs
@@ -1,15 +1,13 @@
 use flate2::bufread::GzDecoder;
 use itertools::Itertools;
-use ndarray::prelude::*;
-use ndarray::Array;
-use ndarray::IntoDimension;
-use reqwest;
-use reqwest::blocking::Response;
+use ndarray::{iter::AxisIter, prelude::*, Array, IntoDimension, RemoveAxis};
+use reqwest::{self, blocking::Response};
 use std::fs;
 use std::io::{copy, BufReader, Read};
 use std::path::{Path, PathBuf};
 
 use crate::iterator::IteratorExt;
+use crate::prelude::*;
 
 #[allow(clippy::upper_case_acronyms)]
 pub struct MNIST {
@@ -59,22 +57,19 @@ impl MNIST {
         "9 - nine",
     ];
 
-    pub fn new(mut root: PathBuf, train: bool, download: bool) -> Self {
-        root.push("mnist");
-        root.push("raw");
-
+    pub fn new(root: &Path, train: bool, download: bool) -> Self {
         if download {
-            MNIST::download(&root);
+            MNIST::download(root);
         }
 
         assert!(
-            !MNIST::check_exits(&root),
+            MNIST::check_exits(root),
             "Dataset not found. You can set download=true to download it"
         );
 
-        let (data, labels) = MNIST::load_data(&root, train);
+        let (data, labels) = MNIST::load_data(root, train);
         MNIST {
-            root,
+            root: root.to_owned(),
             data,
             labels,
             train,
@@ -150,22 +145,18 @@ impl MNIST {
             Err(_) => false,
             Ok(mut folder) => folder.all_default(false, |f| match f {
                 Err(_) => false,
-                Ok(f) => {
-                    Self::RESOURCES
-                        .iter()
-                        .map(|r| r.1)
-                        .any(|r| match f.file_name().to_str() {
-                            None => false,
-                            Some(f) => r.contains(f),
-                        })
-                }
+                Ok(f) => Self::RESOURCES
+                    .iter()
+                    .any(|r| match f.file_name().to_str() {
+                        None => false,
+                        Some(f) => r.0.contains(f),
+                    }),
             }),
         }
     }
 
     fn load_data(root: &Path, train: bool) -> (Array3<u8>, Array1<u8>) {
         let start = if train { "train" } else { "t10k" };
-
         let mut path = PathBuf::from(root);
         path.push(format!("{start}-images-idx3-ubyte"));
         let data = read_image_file(&path);
@@ -174,7 +165,63 @@ impl MNIST {
         path.push(format!("{start}-labels-idx1-ubyte"));
         let labels = read_label_file(&path);
 
+        assert_eq!(data.len_of(Axis(0)), labels.len_of(Axis(0)));
         (data, labels)
+    }
+}
+
+impl Dataset for MNIST {
+    type Item = (Array2<u8>, Array0<u8>);
+
+    fn get(&self, index: usize) -> Option<Self::Item> {
+        if index > self.len() {
+            return None;
+        }
+
+        Some((
+            self.data.index_axis(Axis(0), index).to_owned(),
+            self.labels.index_axis(Axis(0), index).to_owned(),
+        ))
+    }
+
+    fn len(&self) -> usize {
+        self.labels.len()
+    }
+}
+
+pub struct CombinedIter<'a, T, D, T2, D2> {
+    data: AxisIter<'a, T, D>,
+    labels: AxisIter<'a, T2, D2>,
+}
+
+impl<'a, T, D: Dimension, T2, D2: Dimension> CombinedIter<'a, T, D, T2, D2> {
+    pub(crate) fn new<Di, Di2>(data: &'a Array<T, Di>, labels: &'a Array<T2, Di2>) -> Self
+    where
+        Di: RemoveAxis<Smaller = D>,
+        Di2: RemoveAxis<Smaller = D2>,
+    {
+        Self {
+            data: data.outer_iter(),
+            labels: labels.outer_iter(),
+        }
+    }
+}
+
+impl<'a, T: Clone, D: Dimension, T2: Clone, D2: Dimension> Iterator
+    for CombinedIter<'a, T, D, T2, D2>
+{
+    type Item = (Array<T, D>, Array<T2, D2>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some((self.data.next()?.to_owned(), self.labels.next()?.to_owned()))
+    }
+}
+
+impl<'a> IterableDataset<'a> for MNIST {
+    type Iterator = CombinedIter<'a, u8, Ix2, u8, Ix0>;
+
+    fn iter(&'a self) -> Self::Iterator {
+        CombinedIter::new(&self.data, &self.labels)
     }
 }
 
@@ -253,17 +300,136 @@ fn read_image_file(path: &Path) -> Array3<u8> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use fs_extra::dir::{copy, remove, CopyOptions};
+    use std::cmp::{max, min};
+    use std::sync::Mutex;
+
+    static TESTS_EXECUTED: Mutex<u8> = Mutex::new(0);
+    const TOTAL_TESTS: u8 = 4;
+
+    #[inline]
+    fn data_path(n: u8) -> PathBuf {
+        PathBuf::from(format!(".data_{n}"))
+    }
+    #[inline]
+    fn tmp_path() -> PathBuf {
+        PathBuf::from(".data_tmp")
+    }
+
+    #[inline]
+    fn prepare(loaded_data: bool) -> u8 {
+        let mut lock = TESTS_EXECUTED.lock().unwrap();
+        let n_test = lock.to_owned();
+
+        if n_test == 0 {
+            MNIST::download(&tmp_path());
+        }
+        *lock += 1;
+        // Moves data from
+        if loaded_data {
+            fs::create_dir_all(data_path(n_test)).unwrap();
+
+            let options = CopyOptions::new().content_only(true).overwrite(true);
+            copy(tmp_path(), data_path(n_test), &options).unwrap();
+        }
+        n_test
+    }
+
+    #[inline]
+    fn finish(n: u8) {
+        remove(data_path(n)).unwrap();
+
+        if TESTS_EXECUTED.lock().unwrap().eq(&TOTAL_TESTS) {
+            remove(tmp_path()).unwrap();
+        }
+    }
 
     #[test]
     fn download() {
-        let folder = Path::new("download_dataset");
-        MNIST::download(folder.clone());
+        let n = prepare(false);
 
-        let iter = fs::read_dir(folder.clone()).unwrap();
+        let path = data_path(n);
+        MNIST::download(&path);
+
+        let iter = fs::read_dir(path).unwrap();
         assert_eq!(4, iter.count());
 
-        MNIST::load_data(folder, false);
+        finish(n);
+    }
 
-        fs::remove_dir_all(folder).unwrap();
+    #[test]
+    fn check_exists() {
+        let n = prepare(true);
+
+        let path = data_path(n);
+        assert!(MNIST::check_exits(&path));
+        finish(n);
+
+        assert!(!MNIST::check_exits(&path));
+    }
+
+    #[test]
+    fn load_data() {
+        let n = prepare(true);
+
+        let path = data_path(n);
+        let (data, labels) = MNIST::load_data(&path, false);
+        // Data inputs
+        assert_eq!(10_000, data.len_of(Axis(0)));
+        assert_eq!(10_000, labels.len());
+
+        // Image dimensions
+        assert_eq!(28, data.len_of(Axis(1)));
+        assert_eq!(28, data.len_of(Axis(2)));
+
+        let (data, labels) = MNIST::load_data(&path, true);
+        // Data inputs
+        assert_eq!(60_000, data.len_of(Axis(0)));
+        assert_eq!(60_000, labels.len());
+
+        // Image dimensions
+        assert_eq!(28, data.len_of(Axis(1)));
+        assert_eq!(28, data.len_of(Axis(2)));
+
+        finish(n);
+    }
+
+    #[test]
+    fn new() {
+        let n = prepare(true);
+
+        let path = data_path(n);
+        let data = MNIST::new(&path, false, false);
+
+        assert_eq!(10_000, data.len());
+        let (sample, _) = data.get(5_001).unwrap();
+        assert_eq!(28, sample.len_of(Axis(0)));
+        assert_eq!(28, sample.len_of(Axis(1)));
+
+        let (max_sample, max_label) =
+            data.iter()
+                .fold((u8::MIN, u8::MIN), |(max_s, max_l), (s, l)| {
+                    (
+                        max(max_s, s.into_iter().max().unwrap()),
+                        max(max_l, l.into_scalar()),
+                    )
+                });
+
+        let (min_sample, min_label) =
+            data.iter()
+                .fold((u8::MAX, u8::MAX), |(max_s, max_l), (s, l)| {
+                    (
+                        min(max_s, s.into_iter().min().unwrap()),
+                        min(max_l, l.into_scalar()),
+                    )
+                });
+
+        assert_eq!(255, max_sample);
+        assert_eq!(0, min_sample);
+
+        assert_eq!(9, max_label);
+        assert_eq!(0, min_label);
+
+        finish(n);
     }
 }
